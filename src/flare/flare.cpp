262157144ebb197c6866a43a7cc8af7d33962053
Rong Ou <rong.ou@gmail.com>
2019-03-22 17:48:31 -0700

    skeleton grpc client and server

---
+++ src/flare/flare.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include <grpcpp/grpcpp.h>
+
+#include "flare.grpc.pb.h"
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+using flare::FlareRequest;
+using flare::FlareReply;
+using flare::Flare;
+
+class FlareClient {
+public:
+  FlareClient(std::shared_ptr<Channel> channel)
+      : stub_(Flare::NewStub(channel)) {}
+
+  // Assembles the client's payload, sends it and presents the response back
+  // from the server.
+  std::string Execute(const std::string &sql) {
+    // Data we are sending to the server.
+    FlareRequest request;
+    request.set_sql(sql);
+
+    // Container for the data we expect from the server.
+    FlareReply reply;
+
+    // Context for the client. It could be used to convey extra information to
+    // the server and/or tweak certain RPC behaviors.
+    ClientContext context;
+
+    // The actual RPC.
+    Status status = stub_->Execute(&context, request, &reply);
+
+    // Act upon its status.
+    if (status.ok()) {
+      return reply.result();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC failed";
+    }
+  }
+
+private:
+  std::unique_ptr<Flare::Stub> stub_;
+};
+
+int main(int argc, char **argv) {
+  FlareClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+
+  char *buf;
+  while ((buf = readline("flare> ")) != nullptr) {
+    if (strlen(buf) > 0) {
+      add_history(buf);
+    }
+
+    std::string sql(buf);
+    std::string result = client.Execute(sql);
+    std::cout << result << std::endl;
+
+    free(buf);
+  }
+
+  return 0;
+}
